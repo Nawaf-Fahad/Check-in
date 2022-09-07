57bf1d184b36e9cb9cbc57564a5e715df710680c
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2022-09-07 11:29:21 +0300

    Update LocationListView.swift

--- Check-in/LocationListView.swift
+++ Check-in/LocationListView.swift
@@ -9,12 +9,63 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List{
+                ForEach(0..<10 ,id:\.self){item in
+                    HStack{
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical,6)
+                            
+                        
+                        VStack(alignment:.leading){
+                            Text("Test Resturant is here")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            
+                            HStack{
+                                ImageView()
+                                ImageView()
+                                ImageView()
+                                ImageView()
+                                
+                                
+                            }
+                        }
+                        .padding(.leading)
+                        
+                        
+                    }
+                    
+                 
+                    
+                }
+            }
+            .navigationTitle("List of Spots")
+            .listStyle(.plain)
+//            .frame(maxWidth:.infinity)
+            
+        }
     }
 }
 
 struct LocationListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationListView()
+    }
+}
+
+struct ImageView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
     }
 }
