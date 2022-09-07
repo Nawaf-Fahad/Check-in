b6f33d44b2bbe1ab6a41e23684d7a9e710669a4d
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2022-09-07 06:31:33 +0300

    Initial Commit

---
+++ Check-in/Check_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_inApp.swift
+//  Check-in
+//
+//  Created by Nawaf Alotaibi on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Check_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
